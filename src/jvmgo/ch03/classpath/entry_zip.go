9f8569bad29f833d37bb83f4bb84355b2cd65b89
Winson <laxsong@hotmail.com>
2017-07-01 10:09:50 +0800

    ch04

---
+++ src/jvmgo/ch03/classpath/entry_zip.go
@@ -0,0 +1,62 @@
+package classpath
+
+import "archive/zip"
+import "errors"
+import "io/ioutil"
+import (
+	"path/filepath"
+	"fmt"
+	"strings"
+)
+
+type ZipEntry struct {
+	absPath string
+}
+
+func newZipEntry(path string) *ZipEntry {
+	absPath, err := filepath.Abs(path)
+
+	if err != nil {
+		panic(err)
+	}
+	return &ZipEntry{absPath}
+}
+
+func (self *ZipEntry) readClass(className string) ([]byte, Entry, error) {
+	r, err := zip.OpenReader(self.absPath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	fmt.Println("To find from:" + self.absPath)
+
+	defer r.Close()
+
+	for _, f := range r.File {
+		if strings.HasPrefix(f.Name, "com/lang") {
+		fmt.Printf("=====\n")
+		fmt.Print(f.Name);
+		fmt.Printf("=====\n")
+	}
+		if f.Name == className {
+			rc, err := f.Open()
+			if err != nil {
+				return nil, nil, err
+			}
+
+			defer rc.Close()
+			data, err := ioutil.ReadAll(rc)
+			if err != nil {
+				return nil, nil, err
+			}
+
+			return data, self, nil
+		}
+	}
+
+	return nil, nil, errors.New("class not found: " + className)
+}
+
+func (self *ZipEntry) String() string {
+	return self.absPath
+}
---
+++ src/jvmgo/ch04/classfile/constant_pool.go
@@ -0,0 +1,96 @@
+package classfile
+
+/**
+Note:
+1.The u2(constant_pool_count) in .class defines how many counstant there in the pool,but it counts itself included;
+2. The start index of the coustant pool is 1, rather than 0, because it takes u2(constant_pool_count) in;
+3. LONG and Double takes 2 times of storage of Integer
+*/
+type ConstantPool []ConstantInfo
+
+func readConstantPool(reader *ClassReader) ConstantPool {
+
+	cpCount := int(reader.readUint16())
+	cp := make([]ConstantInfo, cpCount)
+
+	for i := 1; i < cpCount; i++ {
+		cp[i] = readConstantInfo(reader, cp)
+		switch cp[i].(type) {
+		case *ConstantLongInfo, *ConstantDoubleInfo:
+			i++
+		}
+	}
+	return cp
+}
+
+func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo {
+	if cpInfo := self[index]; cpInfo != nil {
+		return cpInfo
+	}
+	panic("Invalid constant pool index!")
+}
+
+func (self ConstantPool) getNameAndType(index uint16) (string, string) {
+	ntInfo := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+	name := self.getUtf8(ntInfo.nameIndex)
+	_type := self.getUtf8(ntInfo.descriptorIndex)
+	return name, _type
+
+}
+
+func (self ConstantPool) getClassName(index uint16) string {
+	classInfo := self.getConstantInfo(index).(*ConstantClassInfo)
+	return self.getUtf8(classInfo.nameIndex)
+}
+
+func (self ConstantPool) getUtf8(index uint16) string {
+	utf8Info := self.getConstantInfo(index).(*ConstantUtf8Info)
+	return utf8Info.str
+}
+
+type ConstantInfo interface {
+	readInfo(reader *ClassReader)
+}
+
+func readConstantInfo(reader *ClassReader, cp ConstantPool) ConstantInfo {
+	tag := reader.readUint8()
+	c := newConstantInfo(tag, cp)
+	c.readInfo(reader)
+	return c
+}
+
+func newConstantInfo(tag uint8, cp ConstantPool) ConstantInfo {
+	switch tag {
+	case CONSTANT_Integer:
+		return &ConstantIntegerInfo{}
+	case CONSTANT_Float:
+		return &ConstantIntegerInfo{}
+	case CONSTANT_Long:
+		return &ConstantLongInfo{}
+	case CONSTANT_Double:
+		return &ConstantDoubleInfo{}
+	case CONSTANT_Utf8:
+		return &ConstantUtf8Info{}
+	case CONSTANT_String:
+		return &ConstantStringInfo{}
+	case CONSTANT_Class:
+		return &ConstantClassInfo{}
+	case CONSTANT_Fieldref:
+		return &ConstantFieldrefInfo{ConstantMemberrefInfo{cp: cp}}
+	case CONSTANT_Methodref:
+		return &ConstantMethodrefInfo{ConstantMemberrefInfo{cp: cp}}
+	case CONSTANT_InterfaceMethodref:
+		return &ConstantInterfaceMethodrefInfo{ConstantMemberrefInfo{cp: cp}}
+	case CONSTANT_NameAndType:
+		return &ConstantNameAndTypeInfo{}
+		/*
+			case CONSTANT_MethodType:
+				return &CONSTANT_MethodType
+
+			case CONSTANT_Methodhandle: return &CONSTANT_Methodhandle
+			case
+		*/
+	default:
+		panic("java.lang.ClassFormatError: constant pool tag!")
+	}
+}
